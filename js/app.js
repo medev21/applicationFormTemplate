b04100af8f7d9529d360d9d20236b2e5324f43ad
Martin Ben <mebenav88@gmail.com>
2016-01-08 08:54:48 -0500

    add switch case to select media link for respected input field

--- js/app.js
+++ js/app.js
@@ -1,35 +1,24 @@
-// $(document).ready(function() {
-//     var max_fields      = 10; //maximum input boxes allowed
-//     var wrapper         = $(".input_fields_wrap"); //Fields wrapper
-//     var add_button      = $(".add_field_button"); //Add button ID
-//
-//     var x = 1; //initlal text box count
-//     $(add_button).click(function(e){ //on add input button click
-//         e.preventDefault();
-//         if(x < max_fields){ //max input box allowed
-//             x++; //text box increment
-//             $(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
-//         }
-//     });
-//
-//     $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
-//         e.preventDefault(); $(this).parent('div').remove(); x--;
-//     })
-// });
-
-// <div class="input-group">
-//   <span class="input-group-addon" id="basic-addon1">@</span>
-//   <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
-// </div>
-
 var mediaWrapper = $('.media-fields');
 var addButton = $('.add-media');
 
-$(addButton).click(function(){
-  $("li").click(function(){
-    console.log('button clicked!');
-    $(mediaWrapper).append("<div class='input-group'><span class='input-group-addon' id='basic-addon1'><i class='fa fa-linkedin-square'></i></span><input type='text' class='form-control' placeholder='Username' aria-describedby='basic-addon1'><a href='#' class='remove-media-input'>X</a></div>");
+$(addButton).click(function(event){
+
+  $("li").unbind().click(function(){
+    // unbind() -> To make sure a click only actions once use this
+    var className = $(this).attr('class');
+    switch (className) {
+      case 'linkedin-link':
+        console.log('linkedin clicked');
+        $(mediaWrapper).append("<div class='input-group'><span class='input-group-addon' id='basic-addon1'><i class='fa fa-linkedin-square'></i></span><input type='text' class='form-control' placeholder='Username' aria-describedby='basic-addon1'><a href='#' class='remove-media-input'>X</a></div>");
+        break;
+      case 'github-link':
+        console.log('github cliked');
+        break;
+      default:
+        console.log('default clicked');
+    }
   });
+
 });
 
 $(mediaWrapper).on("click", ".remove-media-input", function(e){
